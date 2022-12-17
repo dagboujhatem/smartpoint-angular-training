2283c150c3d1a92a1eb5f6f680789e8f4039c0af
dagbouj hatem <dagboujhatem@gmail.com>
2022-12-17 16:52:08 +0100

    change custom directive

--- src/app/custom-directive/directives/card-hover.directive.ts
+++ src/app/custom-directive/directives/card-hover.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 // custom attribute derective
 // using cc as a namespace of this directive
@@ -16,8 +16,23 @@ export class CardHoverDirective {
     // this.el.nativeElement.style.setProperty("background-color", "black", "important");
     // this.el.nativeElement.style.color= 'red';
     // with renderer service
+    // this.renderer.setStyle(this.el.nativeElement, 'backgroundColor','black');
+    // this.renderer.setStyle(this.el.nativeElement,'color', 'red');
+  }
+
+  @HostListener('mouseover') // function decorator to detect event of nativeElement: like hover
+  onHover() {
+    // mouse hover event
+    // window.alert("hover");
     this.renderer.setStyle(this.el.nativeElement, 'backgroundColor','black');
     this.renderer.setStyle(this.el.nativeElement,'color', 'red');
+  }
+
+  @HostListener('mouseout')
+  onMouseOut() {
+    // mouseout event
+    this.renderer.setStyle(this.el.nativeElement, 'backgroundColor','white');
+    this.renderer.setStyle(this.el.nativeElement,'color', 'black');
   }
 
 
